0fcc72de0afcec57634d3f00b6bf8831c65109cc
John <99135595+m6jgj@users.noreply.github.com>
2022-10-06 18:54:58 +0100

    Update noip-duc-linux.sh

--- noip-duc-linux.sh
+++ noip-duc-linux.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd /usr/local/src
-wget https://www.noip.com/client/linux/noip-duc-linux.tar.gz
-tar xzf noip-duc-linux.tar.gz
-cd no-ip-2.1.9
-make
-make install
-#rm noip-duc-linux.sh
+sudo wget https://www.noip.com/client/linux/noip-duc-linux.tar.gz
+sudo tar xzf noip-duc-linux.tar.gz
+sudo rm noip-duc-linux.tar.gz
+cd noip-2.1.9-1
+sudo make
+sudo make install
+cd
+rm noip-duc-linux.sh
